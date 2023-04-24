ad28d04826cc7b6a6bdd074c39ff63119df385f6
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-04-24 13:47:03 +0300

    Added movies to the watchlist

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { MovieCard } from './MovieCard'
 
 export const Watchlist = () => {
+  const {watchlist} = useContext(GlobalContext)
   return (
-    <div>
-      <h1>Watchlist Page</h1>
+    <div className='movie-page'>
+      <div className='container'>
+        <div className='header'>
+          <h1 className='heading'>My Watchlist</h1>
+        </div>
+
+        {watchlist.length > 0 ? (
+          <div className="movie-grid">
+          {watchlist.map((movie) => (
+            <MovieCard movie={movie} type='watchlist' />
+          ))}
+        </div>
+          ) : (
+            <div className="no-movies">No movies in your list!</div>
+          )}
+      </div>
     </div>
   )
 }
