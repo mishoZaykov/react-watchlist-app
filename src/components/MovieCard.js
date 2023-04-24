a7716f1a9343c4f7bec8be01a3d59d1ec67ffa4f
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-04-24 14:00:46 +0300

    Added remove button to remove movies from watchlist

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { MovieControls } from './MovieControls'
+
 export const MovieCard = ({movie, type}) => {
   return (
     <div className='movie-card'>
       <div className='overlay'></div>
-      
+
       {movie.poster_path ? (
           <img 
           src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} 
@@ -13,6 +15,8 @@ export const MovieCard = ({movie, type}) => {
           ) : (
             <div className='filler-poster'></div>
           )}
+
+      <MovieControls type={type} movie={movie} />
     </div>
   )
 }
